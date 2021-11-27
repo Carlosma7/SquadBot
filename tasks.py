4832d1e746fd2f6653928f7e1dd42639162a8bc5
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-11-27 19:18:09 +0100

    Close #5 Invoke tasks file definition

---
+++ tasks.py
@@ -0,0 +1,14 @@
+from invoke import task, run
+
+# File cleaning task
+@task 
+def clean(c):
+	print("Cleaning python caché.")
+	run("find . -maxdepth 4 -type d -name  .pytest_cache -exec rm -r {} +")
+	run("find . -maxdepth 4 -type d -name __pycache__ -exec rm -r {} +")
+
+# Testing execution task
+@task
+def test(c):
+	print("Tests execution from SquadBot")
+	run("pytest -v --disable-pytest-warnings src/test/*")
