0fd84ce4f990ec1bd2e54fece9e526f23ae3ba41
Carlos-Morales <carlos7ma@correo.ugr.es>
2021-11-28 21:07:46 +0100

    #12 Chat management and setters/getters definition

--- src/core/controller.py
+++ src/core/controller.py
@@ -4,6 +4,7 @@ from exceptions import *
 import os
 from dotenv import load_dotenv
 import pymongo
+import datetime
 
 # Obtain info from .env
 load_dotenv(dotenv_path = '.env')
@@ -136,3 +137,126 @@ class Controller():
 			return ss.get_pairs()
 		else:
 			raise NonExistingSecretSanta('There is not an open Secret Santa in the group chat. Please create one first then join it.')
+
+	# Get a Secret Santa party object
+	def get_secret_santa(self, chat: str):
+		"""Returns the whole instance of a Secret Santa party from the chat if it exists."""
+
+		# Check there's an open Secret Santa party in the group chat
+		ss = self.mongo.secretsantas.find_one({'chat': chat})
+		ss_found = (ss != None)
+
+		if ss_found:
+			ss = SecretSanta.from_dict(ss)
+
+			return ss
+		else:
+			raise NonExistingSecretSanta('There is not an open Secret Santa in the group chat. Please create one first then join it.')
+
+	# Remove Secret Santa party
+	def remove_secret_santa(self, chat: str):
+		"""Removes all the information from the Secret Santa."""
+
+		# Check there's an open Secret Santa party in the group chat
+		ss = self.mongo.secretsantas.find_one({'chat': chat})
+		ss_found = (ss != None)
+
+		if ss_found:
+			# Remove Secret Santa party from database
+			self.mongo.secretsantas.delete_one({'chat': chat})
+		else:
+			raise NonExistingSecretSanta('There is not an open Secret Santa in the group chat. Please create one first then join it.')
+
+	# Set the limit for the Secret Santa party
+	def set_limit_secret_santa(self, chat: str, limit: int):
+		"""Sets the limit of price for the Secret Santa."""
+
+		# Check there's an open Secret Santa party in the group chat
+		ss = self.mongo.secretsantas.find_one({'chat': chat})
+		ss_found = (ss != None)
+
+		if ss_found:
+			ss = SecretSanta.from_dict(ss)
+
+			# Set the new limit price
+			ss.set_limit(limit)
+
+			# Update in database
+			self.mongo.secretsantas.update({'chat': ss.get_chat()}, {'$set': ss.to_dict()})
+		else:
+			raise NonExistingSecretSanta('There is not an open Secret Santa in the group chat. Please create one first then join it.')
+
+	# Set the date for the Secret Santa party
+	def set_date_secret_santa(self, chat: str, date: datetime.datetime):
+		"""Sets the date for the Secret Santa."""
+
+		# Check there's an open Secret Santa party in the group chat
+		ss = self.mongo.secretsantas.find_one({'chat': chat})
+		ss_found = (ss != None)
+
+		if ss_found:
+			ss = SecretSanta.from_dict(ss)
+
+			# Set the new date
+			ss.set_date(date)
+
+			# Update in database
+			self.mongo.secretsantas.update({'chat': ss.get_chat()}, {'$set': ss.to_dict()})
+		else:
+			raise NonExistingSecretSanta('There is not an open Secret Santa in the group chat. Please create one first then join it.')
+
+	# Set the message for the Secret Santa party
+	def set_message_secret_santa(self, chat: str, message: str):
+		"""Sets the message for the Secret Santa."""
+
+		# Check there's an open Secret Santa party in the group chat
+		ss = self.mongo.secretsantas.find_one({'chat': chat})
+		ss_found = (ss != None)
+
+		if ss_found:
+			ss = SecretSanta.from_dict(ss)
+
+			# Set the new message
+			ss.set_message(message)
+
+			# Update in database
+			self.mongo.secretsantas.update({'chat': ss.get_chat()}, {'$set': ss.to_dict()})
+		else:
+			raise NonExistingSecretSanta('There is not an open Secret Santa in the group chat. Please create one first then join it.')
+
+	# Save the private chat id from an user
+	def save_chat(self, chat: str, username: str):
+		"""Saves the private chat id from a conversation with an user to contact him/her later."""
+
+		# Check there is a private chat already for that user
+		user = self.mongo.chats.find_one({'username': username})
+		user_found = (user != None)
+
+		if user_found:
+			# Modify the private chat id related to the user
+			user['chat'] = chat
+
+			# Update in database
+			self.mongo.chats.update({'username': username}, {'$set': user})
+
+		else:
+			# Save the private chat id related to the user
+			user = {'username': username, 'chat': chat}
+
+			# Save in database
+			self.mongo.chats.insert_one(user)
+
+	# Get the private chat id from an user
+	def get_chat(self, username: str):
+		"""Gets the private chat id from a conversation with an user to contact him/her."""
+
+		# Check there is a private chat already for that user
+		user = self.mongo.chats.find_one({'username': username})
+		user_found = (user != None)
+
+		if user_found:
+
+			return user['chat']
+
+		else:
+			raise UserHasNotStartedBotError('{username} cannot be contacted because he has not started the bot in a private conversation.')
